8903b6fd3cb9600ad4d27aec9cfd86f1958b16b8
nutdnuy <46078640+nutdnuy@users.noreply.github.com>
2023-07-19 20:15:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,16 +27,15 @@ with col2:
 
 df = pd.read_csv('ESG_Score.csv')
 
-
-
-
-
 df['dummy_W'] = np.where(df['ESG Combined Score']  > 0, 1, 0)
 
+df_emission = pd.read_csv("Mar_Emission.csv")
+
+df_emission["Scope_1"] = df_emission["CO2 Equivalent Emissions Direct, Scope 1"] / df_emission["Company Market Cap"]
+df_emission["Scope_2"] = df_emission["CO2 Equivalent Emissions Indirect, Scope 2"] / df_emission["Company Market Cap"]
 
 
 list_AssetClass =  [""] + df["ธรแาพ"].tolist()
-
 
 
 with col1:
@@ -74,6 +73,9 @@ if show_btn:
     ESG_Score_S = 0 
     ESG_Score_G = 0 
     ESG_W = 0
+    
+    Scop1_CO2 = 0
+    Scop2_CO2 = 0
 
 
 
@@ -85,6 +87,9 @@ if show_btn:
             ESG_Score_S += df[df["ธรแาพ"] == Asset_1 ]["Social Pillar Score"].tolist()[0] * Aweight_1 
             ESG_Score_G += df[df["ธรแาพ"] == Asset_1 ]["Governance Pillar Score"].tolist()[0] * Aweight_1 
             ESG_W += Aweight_1 
+            
+            Scop1_CO2 += df_emission[df_emission["Instrument"] == Asset_1]["Scope_1"].tolist()[0] * Aweight_1 /100
+            Scop2_CO2 += df_emission[df_emission["Instrument"] == Asset_1]["Scope_2"].tolist()[0] * Aweight_1 /100
 
 
 
@@ -96,6 +101,9 @@ if show_btn:
             ESG_Score_S += df[df["ธรแาพ"] == Asset_2 ]["Social Pillar Score"].tolist()[0] * Aweight_2 
             ESG_Score_G += df[df["ธรแาพ"] == Asset_2 ]["Governance Pillar Score"].tolist()[0] * Aweight_2 
             ESG_W += Aweight_2
+            
+            Scop1_CO2 += df_emission[df_emission["Instrument"] == Asset_2]["Scope_1"].tolist()[0] * Aweight_2 /100
+            Scop2_CO2 += df_emission[df_emission["Instrument"] == Asset_2]["Scope_2"].tolist()[0] * Aweight_1 /100
 
 
 
@@ -106,6 +114,8 @@ if show_btn:
             ESG_Score_S += df[df["ธรแาพ"] == Asset_3 ]["Social Pillar Score"].tolist()[0] * Aweight_3
             ESG_Score_G += df[df["ธรแาพ"] == Asset_3 ]["Governance Pillar Score"].tolist()[0] * Aweight_3
             ESG_W += Aweight_3
+            Scop1_CO2 += df_emission[df_emission["Instrument"] == Asset_3]["Scope_1"].tolist()[0] * Aweight_3 /100
+            Scop2_CO2 += df_emission[df_emission["Instrument"] == Asset_3]["Scope_2"].tolist()[0] * Aweight_1 /100
 
 
 
@@ -118,6 +128,8 @@ if show_btn:
             ESG_Score_S += df[df["ธรแาพ"] == Asset_4 ]["Social Pillar Score"].tolist()[0] * Aweight_4 
             ESG_Score_G += df[df["ธรแาพ"] == Asset_4 ]["Governance Pillar Score"].tolist()[0] * Aweight_4
             ESG_W += Aweight_4
+            Scop1_CO2 += df_emission[df_emission["Instrument"] == Asset_4]["Scope_1"].tolist()[0] * Aweight_4 /100
+            Scop2_CO2 += df_emission[df_emission["Instrument"] == Asset_4]["Scope_2"].tolist()[0] * Aweight_1 /100
 
 
 
@@ -129,9 +141,15 @@ if show_btn:
             ESG_Score_S += df[df["ธรแาพ"] == Asset_5 ]["Social Pillar Score"].tolist()[0] * Aweight_5
             ESG_Score_G += df[df["ธรแาพ"] == Asset_5 ]["Governance Pillar Score"].tolist()[0] * Aweight_5
             ESG_W += Aweight_5
+            
+            Scop1_CO2 += df_emission[df_emission["Instrument"] == Asset_5]["Scope_1"].tolist()[0] * Aweight_5 /100
+            Scop2_CO2 += df_emission[df_emission["Instrument"] == Asset_5]["Scope_2"].tolist()[0] * Aweight_1 /100
         
         
     ESGScore_Port = ESG_Score / ESG_W 
+    
+    Scop1_CO2_all = Scop1_CO2 * Investment_Amount
+    Scop2_CO2_all = Scop2_CO2 * Investment_Amount
     
     
     
@@ -140,37 +158,46 @@ if show_btn:
     with tab1:
     
         col0, col1 = st.columns(2)
+        
+        if ESG_W > 0:
 
-        with col0:
-            st.title(' ESG Port :national_park:  ')
-            st.title('   ')
-            st.title(int(ESGScore_Port))
-            st.title('   ')
-            st.subheader('Environmental Pillar Score')
-            st.subheader(' ')
-            st.subheader('Social Pillar Score')
-            st.subheader(' ')
-            st.subheader('Governance Pillar Score')
-            st.subheader(' ')
-            st.subheader(' ')
+            with col0:
+                st.title(' ESG Port :national_park:  ')
+                st.title('   ')
+                st.title(int(ESGScore_Port))
+                st.title('   ')
+                st.subheader('Environmental Pillar Score')
+                st.subheader(' ')
+                st.subheader('Social Pillar Score')
+                st.subheader(' ')
+                st.subheader('Governance Pillar Score')
+                st.subheader(' ')
+                st.subheader(' ')
 
-        with col1:
-            st.title('%Port cal ESG ')
-            st.title('   ')
-            st.title(int(ESG_W) )
-            st.title('   ')
+            with col1:
+                st.title('%Port cal ESG ')
+                st.title('   ')
+                st.title(int(ESG_W) )
+                st.title('   ')
 
-            st.subheader(int(ESG_Score_E  / ESG_W ))
-            st.subheader(' ')
-            st.subheader(int(ESG_Score_S  / ESG_W) )
-            st.subheader(' ')
-            st.subheader(int(ESG_Score_G / ESG_W ))
-            st.subheader(' ')
-            st.subheader(' ')
+                st.subheader(int(ESG_Score_E  / ESG_W ))
+                st.subheader(' ')
+                st.subheader(int(ESG_Score_S  / ESG_W) )
+                st.subheader(' ')
+                st.subheader(int(ESG_Score_G / ESG_W ))
+                st.subheader(' ')
+                st.subheader(' ')
+        else :
+            st.title(' Error ไม่มี Score  ')
+            
 
     
     
     with tab2:
-        st.header("Coming soon")
-    
+        st.header("CO2 Emission Scope 1" )
+        st.subheader(str(int(Scop1_CO2_all)) + " T.")
+        st.header("CO2 Emission Scope 2" )
+        st.subheader( str(int(Scop2_CO2_all)) + "  T.")
+
+
     
